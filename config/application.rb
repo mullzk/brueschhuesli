020abb3150835f4790da2f06c07ce0559cd67226
Stefan Mueller <info@mullzk.ch>
2019-09-24 15:36:17 +0200

    Forcing SSL in env:prod

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,7 @@ module Brueschhuesli
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-  end
+
+    config.filter_parameters += [:password]
+    end
 end
